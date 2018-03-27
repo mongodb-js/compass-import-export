485b1d793d073c4aaff35048b286258471a3118d
Durran Jordan <durran@gmail.com>
2018-03-27 16:58:06 +0200

    COMPASS-2617: First part header row not imported

--- src/utils/split-lines-transform.js
+++ src/utils/split-lines-transform.js
@@ -37,7 +37,6 @@ class SplitLines extends Transform {
     this[kSource] = this[kSource].concat(chunk);
     if (this.isFirstRecord && this.type === FILE_TYPES.CSV) {
       this.keys = this[kSource].split('\n')[0].split(',');
-      this.isFirstRecord = false;
     }
     if (this[kSource].indexOf('\n') > -1) {
       const lines = this[kSource].split('\n').filter(Boolean);
@@ -45,6 +44,11 @@ class SplitLines extends Transform {
       if (this.isLastLineComplete(lines[lines.length - 1])) {
         this[kSource] = '';
         try {
+          // Drop the header row for CSV.
+          if (this.isFirstRecord && this.type === FILE_TYPES.CSV) {
+            lines.splice(0, 1);
+            this.isFirstRecord = false;
+          }
           const parsedLines = (this.type === FILE_TYPES.JSON)
             ? lines.map(this.parseJsonLine)
             : lines.map(this.toCSV);
@@ -54,6 +58,11 @@ class SplitLines extends Transform {
         }
       }
       const linesToWrite = lines.splice(0, lines.length - 1);
+      console.log(linesToWrite);
+      if (this.isFirstRecord && this.type === FILE_TYPES.CSV) {
+        linesToWrite.splice(0, 1);
+      }
+      this.isFirstRecord = false;
       this[kSource] = lines[0];
       try {
         const parsedLines = (this.type === FILE_TYPES.JSON)
@@ -80,7 +89,7 @@ class SplitLines extends Transform {
     if (this.type === FILE_TYPES.JSON) {
       return this.isJSON(line);
     }
-    return line.endsWith('\n');
+    return line.match(/(.+)$/g) !== null;
   }
 
   toCSV = (line) => {
@@ -88,7 +97,9 @@ class SplitLines extends Transform {
     const obj = {};
     this.keys.forEach(
       (key, i) => {
-        obj[key] = this.isJSON(values[i]) ? this.parseJsonLine(values[i]) : values[i];
+        if (key !== '') {
+          obj[key] = values[i];
+        }
       }
     );
     return obj;
