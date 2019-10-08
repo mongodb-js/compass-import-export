ba59bc78d1f5b8bae61afa9bdd7cd19779f7dc97
Lucas Hrabovsky <hrabovsky.lucas@gmail.com>
2019-10-08 02:20:32 +0200

    getting to the bottom of transient node driver errors

--- src/utils/collection-stream.js
+++ src/utils/collection-stream.js
@@ -1,5 +1,6 @@
 import { Writable } from 'stream';
 import { createLogger } from './logger';
+const backoff = require('backoff');
 const debug = createLogger('collection-stream');
 
 class WritableCollectionStream extends Writable {
@@ -9,7 +10,27 @@ class WritableCollectionStream extends Writable {
     this.ns = ns;
     this.BATCH_SIZE = 1000;
     this.docsWritten = 0;
+    this._initBatch();
+    this._batchCounter = 0;
+    this._stats = {
+      nInserted: 0,
+      nMatched: 0,
+      nModified: 0,
+      nRemoved: 0,
+      nUpserted: 0,
+      ok: 0,
+      writeErrorCount: 0
+    };
+
+    this._errors = [];
+  }
+
+  _initBatch() {
     this.batch = this._collection().initializeOrderedBulkOp();
+    // this.batch.handleWriteError(function(callback, writeResult) {
+    //   console.log('handleWriteError', callback, writeResult);
+    //   callback(null, writeResult);
+    // });
   }
 
   _collection() {
@@ -23,35 +44,115 @@ class WritableCollectionStream extends Writable {
        * TODO: lucas: expose finer-grained bulk op results:
        * https://mongodb.github.io/node-mongodb-native/3.3/api/BulkWriteResult.html
        */
-      return this.batch.execute((err, res) => {
+
+      const nextBatch = (err, res = {}) => {
         this.docsWritten += this.batch.length;
-        this.batch = this._collection().initializeOrderedBulkOp();
         if (err) {
-          debug('error', err);
-          return next(err);
+          debug('batch %s result', this._batchCounter, err, res);
         }
-        debug('batch result', res);
+        this.captureStatsForBulkResult(err, res);
+
+        this._batchCounter++;
+        this._initBatch();
         next();
+      };
+
+      const execBatch = cb => {
+        this.batch.execute(
+          {
+            w: 1
+            // wtimeout: 0 /* 60 * 1000*/
+          },
+          (err, res) => {
+            if (err) {
+              if (err.errorLabels && Array.isArray(err.errorLabels)) {
+                err.message =
+                  'NOTE: @lucas: this is a transient transaction error and will be retried. - ' +
+                  err.message;
+              }
+              this._errors.push(err);
+              console.error(err);
+              console.log(res);
+              return cb(err);
+            }
+            cb(null, res);
+          }
+        );
+      };
+
+      const call = backoff.call(execBatch, nextBatch);
+      call.setStrategy(
+        new backoff.ExponentialStrategy({
+          randomisationFactor: 0,
+          initialDelay: 500,
+          maxDelay: 10000
+        })
+      );
+      call.on('backoff', (number, delay) => {
+        debug(
+          'Batch %s retry #%s failed.  retrying in %sms...',
+          this._batchCounter,
+          number,
+          delay
+        );
       });
+      call.failAfter(5);
+      call.start();
+      return;
     }
     next();
   }
 
   _final(callback) {
     debug('running _final()');
+
     if (this.batch.length === 0) {
-      debug('nothing left in buffer');
+      // debug('nothing left in buffer');
       debug('%d docs written', this.docsWritten);
+      this.printJobStats();
       return callback();
     }
     debug('draining buffered docs', this.batch.length);
     this.batch.execute((err, res) => {
+      this.captureStatsForBulkResult(err, res);
       this.docsWritten += this.batch.length;
+      this.printJobStats();
       this.batch = null;
-      debug('buffer drained', err, res);
+      // debug('buffer drained', err, res);
       debug('%d docs written', this.docsWritten);
       callback(err);
     });
+  }
+
+  captureStatsForBulkResult(err, res) {
+    // TODO: lucas: Still have access to this._batch here.
+    const keys = [
+      'nInserted',
+      'nMatched',
+      'nModified',
+      'nRemoved',
+      'nUpserted',
+      'ok'
+    ];
+
+    keys.forEach(k => {
+      this._stats[k] += res[k] || 0;
+    });
+    if (!err) return;
+
+    if (err.name === 'BulkWriteError') {
+      this._errors.push.apply(this._errors, err.result.result.writeErrors);
+      this._errors.push.apply(
+        this._errors,
+        err.result.result.writeConcernErrors
+      );
+      this._stats.writeErrorCount += err.result.result.writeErrors.length;
+    }
+  }
+
+  printJobStats() {
+    console.table(this._stats);
+    console.log('Errors Seen', this._errors);
   }
 }
 
