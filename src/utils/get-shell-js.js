c9553d5d244d167958ab636c984eb356b0bc03cd
Lucas Hrabovsky <hrabovsky.lucas@gmail.com>
2019-10-25 09:58:13 -0400

    fix: update tests

--- src/utils/get-shell-js.js
+++ src/utils/get-shell-js.js
@@ -3,9 +3,9 @@ import toNS from 'mongodb-ns';
 
 export default function(ns, spec) {
   let ret = `db.${toNS(ns).collection}.find(\n`;
-  ret += '  ' + toJavascriptString(spec.filter, null, '  ');
+  ret += '  ' + toJavascriptString(spec.filter, null, '');
   if (spec.project) {
-    ret += '\n  ' + toJavascriptString(spec.project, null, '  ');
+    ret += ',\n  ' + toJavascriptString(spec.project, null, '');
   }
   ret += '\n)';
   if (spec.limit) {
