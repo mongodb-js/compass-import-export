833c5a5eb54b0c29f747429f4db03754323d0820
Lucas Hrabovsky <hrabovsky.lucas@gmail.com>
2019-10-29 15:31:36 -0400

    preview streams. broke need help

---
+++ src/utils/preview.js
@@ -0,0 +1,36 @@
+import { Writable } from 'stream';
+import peek from 'peek-stream';
+import createParser from './parsers';
+
+import { createLogger } from './logger';
+const debug = createLogger('collection-stream');
+
+export default function({ MAX_SIZE = 10 } = {}) {
+  return new Writable({
+    objectMode: true,
+    highWaterMark: MAX_SIZE,
+    allowHalfOpen: false,
+    write: function(doc, encoding, next) {
+      if (!this.docs) {
+        this.docs = [];
+      }
+      if (this.docs.length < MAX_SIZE) {
+        debug('only have %d docs. Asking for more', this.docs.length);
+        this.docs.push(doc);
+        return next(null);
+      }
+      if (this.docs.length === MAX_SIZE) {
+        debug('reached %d. done!', this.docs.length);
+        return next();
+      }
+      debug('already have max docs.');
+      return next();
+    }
+  });
+}
+
+export const createPeekStream = function(fileType) {
+  return peek({ newline: false, maxBuffer: 64 * 1024 }, function(data, swap) {
+    swap(createParser({ fileType: fileType }));
+  });
+};
